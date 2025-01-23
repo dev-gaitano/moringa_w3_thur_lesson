dbc8069f8d192d6a59399a4329b3b585213a420d
dev-gaitano <dev.gaitano@gmail.com>
2025-01-23 10:23:18 +0300

    Added a function to scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// Equality checks
 let equal = 2 == '2'
 
 console.log(equal) // true;
@@ -9,3 +10,10 @@ console.log(equal) // false;
 equal = 2 === 2
 
 console.log(equal) // true;
+
+// Functions
+function sayHello() {
+  console.log('Hello World!')
+}
+
+sayHello();
